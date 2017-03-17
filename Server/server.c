02db138182aa1d7ea3c2e20245efcb7f54e56312
Jayson Bennett <thisismynavyemail@gmail.com>
2017-03-17 08:06:17 -0400

    Update server.c

--- Server/server.c
+++ Server/server.c
@@ -73,12 +73,13 @@ int main(int argc, char *argv[])
 
 		printf("You have received %d bytes from a client\n", number_of_bytes);
 		printf("\nData received: %s\n",server_buffer);
-		printf("Sending %lu bytes to client\n\nWaiting for next transmission....", strlen(server_buffer));
+		printf("Sending %lu bytes to client\n\n", strlen(server_buffer));
 		number_of_bytes = write(accept_file_descriptor,server_buffer, 4097);
 		if (number_of_bytes < 0)
 		{
 			bad("ERROR writing to socket");
 		}
+		printf("Waiting for next transmission....\n\n");
 		
 	}
 
